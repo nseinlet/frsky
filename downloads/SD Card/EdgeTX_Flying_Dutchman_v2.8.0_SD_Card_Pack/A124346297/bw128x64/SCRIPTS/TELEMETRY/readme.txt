00addb31ab2161f4d318e39707cf1c33af7f3bf5
Nicolas Seinlet <nicolas@seinlet.com>
2024-03-24 15:34:31 +0100

    Revert to OpenTX

---
+++ downloads/SD Card/EdgeTX_Flying_Dutchman_v2.8.0_SD_Card_Pack/A124346297/bw128x64/SCRIPTS/TELEMETRY/readme.txt
@@ -0,0 +1,2 @@
+This directory is for Lua telemetry scripts.
+Those scripts can be selected using DISPLAY screen in MODEL SETUP.
---
+++ updates/SCRIPTS/TOOLS/FlightsHistory.lua
@@ -0,0 +1,7 @@
+-- TNS|Flights History v1.1|TNE
+
+local function run()
+    return "/SCRIPTS/TOOLS/FlightsHistory/main.lua"
+end
+
+return { run = run }
